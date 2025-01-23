e4421683dfd8f6fd43d8d226f8e55d506fac6281
U-Nico-PR <ulisesnicolasp_r_@hotmail.com>
2025-01-22 20:02:10 -0600

    Extracción del campo y se agrego a la lista

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Variables
+let amigos = [];
+
+// Función para el botón Añadir
+function agregarAmigo() {
+    // Captura del campo
+    let nombre = document.getElementById('amigo').value;
+    // Validar campo
+    if (nombre == '') {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        // Se agrega a la lista en caso de no estar vacío
+        amigos.push(nombre);
+        limpiarCampo();
+        agragarAmigo();
+    }
+}
+// Función para limpiar el campo
+function limpiarCampo() {
+    document.querySelector('#amigo').value = '';
+}
+
+function agragarAmigo() {
+    // Falta la implementación para que cada amigo aparezca e la lista.
+}
+// Visualizar lista
+
+// Sorteo aleatorio
